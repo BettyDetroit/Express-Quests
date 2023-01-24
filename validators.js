62c1b5a45866adc665377660d29f9facdccfa94a
BettyDetroit <caubelnelly@gmail.com>
2023-01-24 18:56:31 +0100

    Quete 4 bis with express validator

--- validators.js
+++ validators.js
@@ -1,97 +1,40 @@
-const validateMovie = (req, res, next) => {
-  const { title, director, year, color, duration } = req.body;
-  const errors = [];
+const { body, validationResult } = require('express-validator');
 
-  if (title == null) {
-    errors.push({ field: "title", message: "This field is required" });
-  } else if (title.length >= 255) {
-    errors.push({
-      field: "title",
-      message: "Should contain less than 255 characters",
-    });
-  }
-  if (director == null) {
-    errors.push({ field: "director", message: "This field is required" });
-  } else if (director.length >= 255) {
-    errors.push({
-      field: "director",
-      message: "Should contain less than 255 characters",
-    });
-  }
-  if (year == null) {
-    errors.push({ field: "year", message: "This field is required" });
-  } else if (year.length >= 255) {
-    errors.push({
-      field: "year",
-      message: "Should contain less than 255 characters",
-    });
-  }
-  if (color == null) {
-    errors.push({ field: "color", message: "This field is required" });
-  } else if (color.length >= 255) {
-    errors.push({
-      field: "color",
-      message: "Should contain less than 255 characters",
-    });
-  }
-  if (duration == null) {
-    errors.push({ field: "duration", message: "This field is required" });
-  } else if (title.length !== (1 || 0)) {
-    errors.push({ field: "duration", message: "Should contain 1 or 0" });
-  }
+const validateUser = [
+  body("email").isEmail(),
+  body("firstname").isLength({ max: 255 }),
+  body("lastname").isLength({ max: 255 }),
+  body("city").isLength({ max: 255 }),
+  body("language").isLength({ max: 255 }),
+  (req, res, next) => {
+    const errors = validationResult(req);
 
-  if (errors.length) {
-    res.status(422).json({ validationErrors: errors });
-  } else {
-    next();
-  }
-};
+    if (!errors.isEmpty()) {
+      res.status(422).json({ validationErrors: errors.array() });
+    } else {
+      next();
+    }
+  },
+];
 
-const validateUser = (req, res, next) => {
-  const { firstname, lastname, email, city, language } = req.body;
-  const errors = [];
-
-  if (firstname == null) {
-    errors.push({ field: "firstname", message: "This field is required" });
-  } else if (firstname.length >= 255) {
-    errors.push({
-      field: "firstname",
-      message: "Should contain less than 255 characters",
-    });
-  }
-  if (lastname == null) {
-    errors.push({ field: "lastname", message: "This field is required" });
-  } else if (lastname.length >= 255) {
-    errors.push({
-      field: "lastname",
-      message: "Should contain less than 255 characters",
-    });
-  }
-
-  const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
-
-  if (!emailRegex.test(email)) {
-    errors.push({ field: "email", message: "Invalid email" });
-  }
-
-  if (city == null) {
-    errors.push({ field: "city", message: "This field is required" });
-  } else if (city.length >= 255) {
-      errors.push({ field: "city", message: "Should contain less than 255 characters" });
-    } if (language == null) {
-        errors.push({ field: "language", message: "This field is required" });
-      } else if (language.length >= 255) {
-          errors.push({ field: "language", message: "Should contain less than 255 characters" });
-        }
-
-  if (errors.length) {
-    res.status(422).json({ validationErrors: errors });
-  } else {
-    next();
-  }
-};
+const validateMovie = [
+    body("title").isLength({ max: 255 }),
+    body("director").isLength({ max: 255 }),
+    body("year").isLength({ max: 255 }),
+    body("color").isBoolean(),
+    body("duration").isLength({ max: 255 }),
+    (req, res, next) => {
+      const errors = validationResult(req);
+  
+      if (!errors.isEmpty()) {
+        res.status(422).json({ validationErrors: errors.array() });
+      } else {
+        next();
+      }
+    },
+  ];
 
 module.exports = {
-  validateMovie,
-  validateUser,
-};
+   validateUser,
+   validateMovie,
+  };
